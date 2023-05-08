fd60f87ca7c25c62c0a8f20da6159a157fd0215e
unknown <you@example.com>
2023-05-07 20:08:21 -0600

    segundo avance generación de cuádruplos para expresiones

---
+++ README.md
@@ -0,0 +1,11 @@
+Primer Avance
+Se tomó tarea de little duck y se agregaron cosas extras:
+Sintaxis de funciones, parámetros de funciones, primera parte de arrays.
+Se cambió la forma que lee un archivo, ya acepta tabuladores, saltos de linea y espacios
+Por el momento chequea de buena manera el archivo de prueba que se incluye
+
+Segundo Avance
+Se agregó generación de cuádruplos para expresiones
+Cubo semántico
+Pila de operadores, operandos, tipos
+Y mucho research sobre cómo añadir puntos neurálgicos para analizar semánticamente
--- main.py
+++ main.py
@@ -2,41 +2,87 @@ import ply.lex as lex
 import ply.yacc as yacc
 import sys
 
+cuadruplos = []
+pilaOperadores = []
+pilaOperandos = []
+cubo_semantico = {
+    '*': {
+        'float': {
+            'int': 'float',
+            'float': 'float'
+        },
+        'int': {
+            'int': 'int',
+            'float': 'float'
+        }
+    },
+    '/': {
+        'float': {
+            'int': 'float',
+            'float': 'float'
+        },
+        'int': {
+            'int': 'int',
+            'float': 'float'
+        }
+    },
+    '-': {
+        'float': {
+            'int': 'float',
+            'float': 'float'
+        },
+        'int': {
+            'int': 'int',
+            'float': 'float'
+        }
+    },
+    '+': {
+        'float': {
+            'int': 'float',
+            'float': 'float'
+        },
+        'int': {
+            'int': 'int',
+            'float': 'float'
+        }
+    },
+}
+
 tokens = [
-  'INT_CTE',
-  'FLOAT_CTE',
-  'STRING_CTE',
-  'ID',
-  'PLUS',
-  'MINUS',
-  'DIVIDE',
-  'MULTIPLY',
-  'EQUALS',
-  'COMMA',
-  'SEMICOLON',
-  'COLON',
-  'LEFTPARENTHESES',
-  'RIGHTPARENTHESES',
-  'LEFTBRACE',
-  'RIGHTBRACE',
-  'LEFTBRACKET',
-  'RIGHTBRACKET',
-  'LESSTHAN',
-  'GREATERTHAN',
-  'NOTEQUAL',
+    'INT_CTE',
+    'FLOAT_CTE',
+    'STRING_CTE',
+    'ID',
+    'PLUS',
+    'MINUS',
+    'DIVIDE',
+    'MULTIPLY',
+    'EQUALS',
+    'COMMA',
+    'SEMICOLON',
+    'COLON',
+    'LEFTPARENTHESES',
+    'RIGHTPARENTHESES',
+    'LEFTBRACE',
+    'RIGHTBRACE',
+    'LEFTBRACKET',
+    'RIGHTBRACKET',
+    'LESSTHAN',
+    'GREATERTHAN',
+    'NOTEQUAL',
 ]
 
 reserved = {
-  'Programa': 'PROGRAMA',
-  'Function': 'FUNCTION',
-  'var': 'VAR',
-  'if': 'IF',
-  'else': 'ELSE',
-  'int': 'INT',
-  'float': 'FLOAT',
-  'read': 'READ',
-  'print': 'PRINT',
-  '<>': 'NOTEQUALS',
+    'Programa': 'PROGRAMA',
+    'Function': 'FUNCTION',
+    'var': 'VAR',
+    'if': 'IF',
+    'else': 'ELSE',
+    'int': 'INT',
+    'float': 'FLOAT',
+    'read': 'READ',
+    'print': 'PRINT',
+    '<>': 'NOTEQUALS',
 }
 
 tokens += reserved.values()
@@ -59,98 +105,101 @@ t_LEFTBRACKET = r'\['
 t_RIGHTBRACKET = r'\]'
 t_ignore = ' \n'
 
+
 def t_FLOAT_CTE(t):
-  r'\d+\.\d+'
-  t.value = float(t.value)
-  return t
+    r'\d+\.\d+'
+    t.value = float(t.value)
+    return t
 
 
 def t_INT_CTE(t):
-  r'\d+'
-  t.value = int(t.value)
-  return t
+    r'\d+'
+    t.value = int(t.value)
+    return t
 
 
 def t_STRING_CTE(t):
-  r'\"([^\\\n]|(\\.))*?\"'
-  t.value = str(t.value)
-  return t
+    r'\"([^\\\n]|(\\.))*?\"'
+    t.value = str(t.value)
+    return t
 
 
 def t_PROGRAMA(t):
-  r'Programa'
-  return t
+    r'Programa'
+    return t
+
 
 def t_FUNCTION(t):
-  r'Function'
-  return t
-  
+    r'Function'
+    return t
+
+
 def t_VAR(t):
-  r'var'
-  return t
+    r'var'
+    return t
 
 
 def t_IF(t):
-  r'if'
-  return t
+    r'if'
+    return t
 
 
 def t_FOR(t):
-  r'for'
-  return t
+    r'for'
+    return t
 
 
 def t_THEN(t):
-  r'then'
-  return t
+    r'then'
+    return t
 
 
 def t_ELSE(t):
-  r'else'
-  return t
+    r'else'
+    return t
 
 
 def t_DO(t):
-  r'do'
-  return t
+    r'do'
+    return t
 
 
 def t_TO(t):
-  r'to'
-  return t
+    r'to'
+    return t
 
 
 def t_INT(t):
-  r'int'
-  return t
+    r'int'
+    return t
 
 
 def t_FLOAT(t):
-  r'float'
-  return t
+    r'float'
+    return t
 
 
 def t_READ(t):
-  r'read'
-  return t
+    r'read'
+    return t
 
 
 def t_PRINT(t):
-  r'print'
-  return t
+    r'print'
+    return t
 
 
 def t_ID(t):
-  r'[a-zA-Z_][a-zA-Z_0-9]*'
-  t.type = 'ID'
-  return t
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = 'ID'
+    return t
 
 
 # Mensaje de error en caso de error lexico
 def t_error(t):
-  print(t)
-  print("Illegal characters!")
-  t.lexer.skip(1)
+    print(t)
+    print("Illegal characters!")
+    t.lexer.skip(1)
 
 
 lexer = lex.lex()
@@ -161,223 +210,288 @@ lexer = lex.lex()
 
 
 def p_calc(p):
-  '''
-    calc : PROGRAMA ID SEMICOLON vars function
-         | PROGRAMA ID SEMICOLON function
     '''
-  print('No syntax errors found :)')
+      calc : PROGRAMA ID SEMICOLON vars function
+           | PROGRAMA ID SEMICOLON function
+      '''
+    print('No syntax errors found :)')
 
 
 def p_vars(p):
-  ''' vars : VAR varsaux '''
+    ''' vars : VAR varsaux '''
 
 
 def p_varsaux(p):
-  '''
-    varsaux : ID varsaux2 COLON tipo SEMICOLON varsaux
-            |
     '''
+      varsaux : ID varsaux2 COLON tipo SEMICOLON varsaux
+              |
+      '''
 
 
 def p_varsaux2(p):
-  '''
-    varsaux2 : COMMA ID varsaux2
-            |
     '''
+      varsaux2 : COMMA ID varsaux2
+              |
+      '''
 
 
 def p_tipo(p):
-  '''
-    tipo : INT
-         | FLOAT
     '''
+      tipo : INT
+           | FLOAT
+      '''
 
 
 def p_function(p):
-  '''
-    function : FUNCTION ID params bloque
     '''
+      function : FUNCTION ID params bloque
+      '''
+
 
 def p_params(p):
-  '''
-    params : LEFTPARENTHESES vars RIGHTPARENTHESES
-          | LEFTPARENTHESES RIGHTPARENTHESES
     '''
+      params : LEFTPARENTHESES vars RIGHTPARENTHESES
+            | LEFTPARENTHESES RIGHTPARENTHESES
+      '''
 
 
 def p_bloque(p):
-  '''
-    bloque : LEFTBRACE bloqueaux RIGHTBRACE
     '''
+      bloque : LEFTBRACE bloqueaux RIGHTBRACE
+      '''
 
 
 def p_bloqueaux(p):
-  '''
-    bloqueaux : estatuto bloqueaux 
-            |
     '''
+      bloqueaux : estatuto bloqueaux 
+              |
+      '''
 
 
 def p_estatuto(p):
-  '''
-    estatuto : asignacion
-            | condicion
-            | escritura
     '''
+      estatuto : asignacion
+              | condicion
+              | escritura
+      '''
 
 
 def p_asignacion(p):
-  '''
-    asignacion : ID EQUALS expresion SEMICOLON
     '''
+      asignacion : varcte EQUALS expresion SEMICOLON
+      '''
 
 
 def p_expresion(p):
-  '''
-    expresion : exp expresionaux
     '''
+      expresion : exp expresionaux
+      '''
 
 
 def p_expresionaux(p):
-  '''
-    expresionaux : GREATERTHAN exp
-              | LESSTHAN exp
-              | NOTEQUALS exp
-              |
     '''
+      expresionaux : GREATERTHAN exp
+                | LESSTHAN exp
+                | NOTEQUALS exp
+                |
+      '''
 
 
 def p_escritura(p):
-  '''
-    escritura : PRINT LEFTPARENTHESES escrituraaux RIGHTPARENTHESES SEMICOLON
     '''
+      escritura : PRINT LEFTPARENTHESES escrituraaux RIGHTPARENTHESES SEMICOLON
+      '''
 
 
 def p_escrituraaux(p):
-  '''
-    escrituraaux : expresion
-                | STRING_CTE
-                | expresion COMMA escrituraaux
-                | STRING_CTE COMMA escrituraaux
     '''
+      escrituraaux : expresion
+                  | STRING_CTE
+                  | expresion COMMA escrituraaux
+                  | STRING_CTE COMMA escrituraaux
+      '''
 
 
 def p_exp(p):
-  '''
-            exp : termino
-                | termino expaux
     '''
+              exp : termino seen_termino
+                  | termino seen_termino expaux
+      '''
 
 
 def p_expaux(p):
-  '''
-            expaux : PLUS exp
-                | MINUS exp
     '''
+              expaux : PLUS seen_operador exp
+                  | MINUS seen_operador exp
+      '''
 
 
 def p_termino(p):
-  '''
-          termino : factor
-                | factor terminoaux
     '''
+            termino : factor seen_factor
+                  | factor seen_factor terminoaux
+      '''
 
 
 def p_terminoaux(p):
-  '''
-          terminoaux : DIVIDE termino
-                | MULTIPLY termino
     '''
+            terminoaux : DIVIDE seen_operador termino
+                  | MULTIPLY seen_operador termino
+      '''
 
 
 def p_condicion(p):
-  '''
-      condicion : IF LEFTPARENTHESES expresion RIGHTPARENTHESES bloque condicionaux
     '''
+        condicion : IF LEFTPARENTHESES expresion RIGHTPARENTHESES bloque condicionaux
+      '''
 
 
 def p_condicionaux(p):
-  '''
-      condicionaux : ELSE bloque SEMICOLON
-                | SEMICOLON
     '''
+        condicionaux : ELSE bloque SEMICOLON
+                  | SEMICOLON
+      '''
 
 
 def p_factor(p):
-  '''
-      factor : factoraux
-                | factoraux2
     '''
+        factor : factoraux
+                  | factoraux2
+      '''
 
 
 def p_factoraux(p):
-  '''
-      factoraux : LEFTPARENTHESES expresion RIGHTPARENTHESES
     '''
+        factoraux : LEFTPARENTHESES expresion RIGHTPARENTHESES
+      '''
 
 
 def p_factoraux2(p):
-  '''
-      factoraux2 : factoraux3 varcte
     '''
+        factoraux2 : factoraux3 varcte
+      '''
 
 
-def p_factoraux3(p):
-  '''
-      factoraux3 : PLUS
-                  | MINUS
-                  |
-                
+def p_factoraux3(p):  # QUE SHOW CON ESTO
     '''
+        factoraux3 : PLUS 
+                    | MINUS
+                    |
+
+      '''
 
 
 def p_varcte(p):
-  '''
-      varcte : ID
-                  | INT_CTE
-                  | FLOAT_CTE
-                
     '''
+        varcte : ID seen_ID
+                    | INT_CTE
+                    | FLOAT_CTE
+
+      '''
+
+
+def p_seen_ID(p):
+    "seen_ID :"
+    pilaOperandos.append(p[-1])
+
+
+def p_seen_operador(p):
+    "seen_operador :"
+    pilaOperadores.append(p[-1])
+
+
+def p_seen_termino(p):
+    "seen_termino :"
+    if (len(pilaOperadores) == 0):
+        return
+    if pilaOperadores[-1] == '+' or pilaOperadores[-1] == '-':
+        right_operando = pilaOperandos.pop()
+        left_operando = pilaOperandos.pop()
+        operador = pilaOperadores.pop()
+        result_type = getResultType()
+        if (result_type):
+            result = nextAvail()
+            cuadruplos.append(generate_quad(
+                operador=operador, left_operando=left_operando, right_operando=right_operando, result=result))
+            pilaOperandos.append(result)
+        else:
+            print('ERROR MISMATCH')
+
+
+def p_seen_factor(p):
+    "seen_factor :"
+    if (len(pilaOperadores) == 0):
+        return
+    print(pilaOperadores)
+    print(pilaOperandos)
+
+    if pilaOperadores[-1] == '*' or pilaOperadores[-1] == '/':
+        right_operando = pilaOperandos.pop()
+        left_operando = pilaOperandos.pop()
+        operador = pilaOperadores.pop()
+        result_type = getResultType()
+        if (result_type):
+            result = nextAvail()
+            cuadruplos.append(generate_quad(
+                operador=operador, left_operando=left_operando, right_operando=right_operando, result=result))
+            pilaOperandos.append(result)
+        else:
+            print('ERROR MISMATCH')
 
 
 def p_arrayIntDefinition(p):
-  '''
-      arrayIntDefinition : LEFTBRACKET INT_CTE RIGHTBRACKET
-  '''
+    '''
+        arrayIntDefinition : LEFTBRACKET INT_CTE RIGHTBRACKET
+    '''
 
 
 # Mensaje de error sintactico
 
 
 def p_error(p):
-  print(p)
-  print("Syntax error found!")
+    print(p)
+    print("Syntax error found!")
 
 
 def p_empty(p):
-  '''
-    empty :
     '''
+      empty :
+      '''
 
 
 parser = yacc.yacc()
 
-#Execution
-#IMPORTANT
-#This version does not accept end of lines, so in order to test code please put it in one line, use this tool if you need to convert to one line
-#https://lingojam.com/TexttoOneLine
+
+def generate_quad(operador, left_operando, right_operando, result):
+    return [operador, left_operando, right_operando, result]
+
+
+def nextAvail():
+    return 't1'
+
+
+def getResultType():
+    # result_type = cubo_semantico[operador][left_operando][right_operando]
+    return 'int'
+# Execution
+# IMPORTANT
+# This version does not accept end of lines, so in order to test code please put it in one line, use this tool if you need to convert to one line
+# https://lingojam.com/TexttoOneLine
+
 
 print("1-Load Example from TXT")
 print("2-Input code manually")
 option = input("Option : ")
 if option == "1":
-  file = open("test.txt").read()
-  #print(file)
-  parser.parse(file)
+    file = open("test.txt").read()
+    # print(file)
+    parser.parse(file)
+    print(pilaOperandos)
+    print(cuadruplos)
 else:
-  while True:
-    try:
-      s = input('>> ')
-    except EOFError:
-      break
-    parser.parse(s)
+    while True:
+        try:
+            s = input('>> ')
+        except EOFError:
+            break
+        parser.parse(s)
